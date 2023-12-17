ab1f8e30731275b7fbba17223b43e85deb18fb94
peter <bjkim0426@gmailc.om>
2023-12-17 03:24:56 -0500

    Initial Commit

---
+++ ball.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('white')
+        self.fillcolor('white')
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.75
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.move_speed = 0.1
+    #
+    # def increase_ball_speed(self):
+    #     self.x_move *= 1.05
+    #     self.y_move *= 1.05
+    #
+    # def reset_ball_speed(self):
+    #     self.x_move = 10
+    #     self.y_move = 10
+
+
+
+
+
+
+
